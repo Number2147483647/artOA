08547b3051f937baf7dcd58127a57a10284c8720
OnlyIceSea <1041414957@qq.com>
2018-01-20 13:29:37 +0800

    init

---
+++ artOA/src/main/java/site/binghai/crm/ArtOaApplication.java
@@ -0,0 +1,12 @@
+package site.binghai.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtOaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtOaApplication.class, args);
+	}
+}
