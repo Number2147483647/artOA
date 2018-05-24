77459890e804351a3d9db73a4e6f91fdb24707e4
Number2147483647 <2980024680@qq.com>
2018-05-24 12:37:03 +0800

    up

--- src/main/java/site/binghai/crm/config/Config.java
+++ src/main/java/site/binghai/crm/config/Config.java
@@ -4,5 +4,5 @@ package site.binghai.crm.config;
  * Created by Administrator on 2018/4/30.
  */
 public class Config {
-    public static final String server="http://127.0.0.1:8080";
+    public static final String server="http://art.nanayun.cn";
 }
